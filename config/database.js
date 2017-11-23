2296bd122fd469c297ac4ab8ce9607f05cc75333
Chace Kim <chace@developersquared.co.nz>
2017-11-23 13:13:25 +1300

    initial commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'clinkdbinstance.cjm4fnbo0bnq.ap-northeast-2.rds.amazonaws.com',
+        'user': 'admin',
+        'password': 'clink##1'
+    },
+	'database': 'db_contek_grout',
+    'users_table': 'tb_user'
+};
